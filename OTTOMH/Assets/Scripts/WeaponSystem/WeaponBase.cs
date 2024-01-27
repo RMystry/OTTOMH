ae2918026894e6a6f6c3d92edbd63709e3e49aa5
Rafidh Chowdhury <saucyspacefries@gmail.com>
2024-01-27 15:14:03 -0500

    Weapon Update

--- OTTOMH/Assets/Scripts/WeaponSystem/WeaponBase.cs
+++ OTTOMH/Assets/Scripts/WeaponSystem/WeaponBase.cs
@@ -2,17 +2,23 @@
 
 namespace GGJ
 {
-    public abstract class WeaponBase : MonoBehaviour
+    public abstract class WeaponBase<T> : MonoBehaviour where T : WeaponDescriptor
     {
-        public abstract bool Attack(Vector3 position, out Collider collision);
+        public abstract bool Attack(Vector3 position, T descriptor, out Collider[] collision);
     }
 
-    public class RangedWeapon : WeaponBase
+    public class RangedWeapon : WeaponBase<RangedWeaponDescriptor>
     {
 
-        public override bool Attack(Vector3 position, out Collider collision)
+        public override bool Attack(Vector3 position, RangedWeaponDescriptor descriptor, out Collider[] collision)
         {
+            var shootPosition = GameManager.Player.transform.position + new Vector3(0f, 0.75f, 0f);
             collision = null;
+            if (Physics.Raycast(shootPosition, Vector3.forward, descriptor.range))
+            {
+                return true;
+            }
+
             return false;
         }
     }
