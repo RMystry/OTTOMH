19d29173f53b6c1b4cc801c2efbbfb952aeeebce
Rafidh Chowdhury <saucyspacefries@gmail.com>
2024-01-28 10:29:21 -0500

    OnAttack event added (and particle systems)

--- OTTOMH/Assets/Scripts/WeaponSystem/WeaponBase.cs
+++ OTTOMH/Assets/Scripts/WeaponSystem/WeaponBase.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace GGJ
 {
     public abstract class WeaponBase<T> : MonoBehaviour where T : WeaponDescriptor
     {
+        public ParticleSystem particles;
+        public UnityEvent OnAttack;
         public abstract bool Attack(Vector3 position, T descriptor, out Collider[] collision);
     }
 }
